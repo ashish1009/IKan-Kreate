213f9504c3de545cc5b33814eb6683f17e67cf17
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-27 11:12:11 +0530

    Project Setup
    - Kreator executable
    - IKan Static Library
    - Prefix Headers

---
+++ Kreator/Kreator/EntryPoint.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Kreator
+//
+//  Created by Ashish . on 27/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+  std::cout << "Hello, World!\n";
+  return 0;
+}
