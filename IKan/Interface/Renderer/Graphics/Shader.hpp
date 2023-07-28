a4bd82ed3bb024a76e88e008cc4879c673f16f2f
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-28 22:57:19 +0530

    Adding Usable APIs in Shader Class

--- IKan/Interface/Renderer/Graphics/Shader.hpp
+++ IKan/Interface/Renderer/Graphics/Shader.hpp
@@ -11,6 +11,10 @@
 
 namespace IKan
 {
+  // Forward Declaration
+  class ShaderUniformBufferDeclaration;
+  class ShaderResourceDeclaration;
+
   /// This is the interface class to compile Shader code
   /// - Note: Format of shader file should be
   /// #type vertex
@@ -26,6 +30,86 @@ namespace IKan
   public:
     /// This is the virtual default destructor for Shader
     virtual ~Shader() = default;
+    
+    /// This function binds the Current Shader to GPU
+    virtual void Bind() const = 0;
+    /// This function unbinds the Current Shader to GPU
+    virtual void Unbind() const = 0;
+    
+    /// This function set the vertex shader buffer data
+    /// - Parameter buffer: buffer data
+    virtual void SetVSMaterialUniformBuffer(const Buffer& buffer) = 0;
+    /// This function set the fragment shader buffer data
+    /// - Parameter buffer: buffer data
+    virtual void SetFSMaterialUniformBuffer(const Buffer& buffer) = 0;
+    /// This function set the geomatry shader buffer data
+    /// - Parameter buffer: buffer data
+    virtual void SetGSMaterialUniformBuffer(const Buffer& buffer) = 0;
+    
+    /// This function returns the Name of Shader
+    virtual const std::string& GetName() const = 0;
+    /// This function returns the File Path of Shader
+    virtual const std::string& GetFilePath() const = 0;
+    /// This function returns the Renderer ID of Shader
+    virtual RendererID GetRendererID() const = 0;
+    
+    /// This function returns true if have the vertex shader buffer data
+    virtual bool HasVSMaterialUniformBuffer() const = 0;
+    /// This function returns true if have the fragment shader buffer data
+    virtual bool HasFSMaterialUniformBuffer() const = 0;
+    /// This function returns true if have the geomatry shader buffer data
+    virtual bool HasGSMaterialUniformBuffer() const = 0;
+    
+    /// This function resturns the vertex Shader buffer data
+    virtual const ShaderUniformBufferDeclaration& GetVSMaterialUniformBuffer() const = 0;
+    /// This function resturns the fragment Shader buffer data
+    virtual const ShaderUniformBufferDeclaration& GetFSMaterialUniformBuffer() const = 0;
+    /// This function resturns the geomatry Shader buffer data
+    virtual const ShaderUniformBufferDeclaration& GetGSMaterialUniformBuffer() const = 0;
+    
+    /// This function returns all the resources
+    virtual const std::vector<ShaderResourceDeclaration*>& GetResources() const = 0;
+    
+    // NOTE: For Int and Int arrar (for Sampler 2D) Unform is setting while compiling the shader so no need to call this explicitly
+    
+    /// This functions uploads the Matrix 4x4 array value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - values: Value of Uniform
+    ///   - count: Size of Mat4 Array
+    virtual void SetUniformMat4Array(const std::string& name, const glm::mat4& values, uint32_t count) = 0;
+    
+    /// This functions uploads the Matrix 4x4 value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformMat4(const std::string& name, const glm::mat4& value) = 0;
+    /// This functions uploads the Matrix 3x3 value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformMat3(const std::string& name, const glm::mat3& value) = 0;
+    
+    /// This functions uploads the Flaot value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformFloat1(const std::string& name, float value) = 0;
+    /// This functions uploads the Vec2 value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformFloat2(const std::string& name, const glm::vec2& value) = 0;
+    /// This functions uploads the Vec3 value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformFloat3(const std::string& name, const glm::vec3& value) = 0;
+    /// This functions uploads the Vec4 value to shader
+    /// - Parameters:
+    ///   - name: Name of Uniform
+    ///   - value: Value of Uniform
+    virtual void SetUniformFloat4(const std::string& name, const glm::vec4& value) = 0;
     
     /// This function creates the shader instance based on the renderer API supported by IKan
     /// - Parameter shaderFilePath: Shader file full path
