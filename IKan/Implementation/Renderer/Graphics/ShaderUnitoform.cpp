d4564c46c775682e50246f937b92a67d70bccdfc
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-28 22:13:28 +0530

    Creating Shader Structure class

--- IKan/Implementation/Renderer/Graphics/ShaderUnitoform.cpp
+++ IKan/Implementation/Renderer/Graphics/ShaderUnitoform.cpp
@@ -6,3 +6,52 @@
 //
 
 #include "ShaderUnitoform.hpp"
+
+namespace IKan
+{
+  ShaderStruct::ShaderStruct(const std::string& name)
+  : m_name(name), m_size(0), m_offset(0)
+  {
+    
+  }
+  ShaderStruct::~ShaderStruct()
+  {
+    for (auto& field : m_fields)
+    {
+      ikdelete field;
+    }
+  }
+  
+  void ShaderStruct::SetOffset(uint32_t offset)
+  {
+    m_offset = offset;
+  }
+  
+  uint32_t ShaderStruct::GetSize() const
+  {
+    return m_size;
+  }
+  uint32_t ShaderStruct::GetOffset() const
+  {
+    return m_offset;
+  }
+  const std::string& ShaderStruct::GetName() const
+  {
+    return m_name;
+  }
+  const std::vector<ShaderUniformDeclaration*>& ShaderStruct::GetFields() const
+  {
+    return m_fields;
+  }
+  void ShaderStruct::AddField(ShaderUniformDeclaration* field)
+  {
+    m_size += field->GetSize();
+    uint32_t offset = 0;
+    if (m_fields.size()) {
+      ShaderUniformDeclaration* previous = m_fields.back();
+      offset = previous->GetOffset() + previous->GetSize();
+    }
+    field->SetOffset(offset);
+    m_fields.push_back(field);
+  }
+} // namespace IKan
