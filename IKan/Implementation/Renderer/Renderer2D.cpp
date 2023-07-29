c725b03b97689658c75d0728134463fbeda61fe8
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-29 12:02:24 +0530

    Creating Full Screen Renderer Data

--- IKan/Implementation/Renderer/Renderer2D.cpp
+++ IKan/Implementation/Renderer/Renderer2D.cpp
@@ -6,16 +6,44 @@
 //
 
 #include "Renderer2D.hpp"
+#include "Renderer/Graphics/Shader.hpp"
+#include "Renderer/Graphics/Texture.hpp"
+#include "Renderer/Graphics/Pipeline.hpp"
 
 namespace IKan
 {
+  /// This structures stores the Full Screen quad
+  struct FullScreenQuad
+  {
+    /// Vertices of Full Screen Quad
+    static constexpr float vertices[] =
+    {
+      // positions        // texture Coords
+      -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+      -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+      1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+      1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+    };
+    
+    /// Pipelne to render full screen quad
+    Ref<Pipeline> pipeline;
+    Ref<VertexBuffer> vertexBuffer;
+    
+    /// This function initilaise the full screen quad data
+    FullScreenQuad()
+    {
+
+    }
+  };
+  static Scope<FullScreenQuad> s_fullscreenQuadData;
+  
   void Renderer2D::Initialise()
   {
-    
+    s_fullscreenQuadData = CreateScope<FullScreenQuad>();
   }
   
   void Renderer2D::Shutdown()
   {
-    \
+    s_fullscreenQuadData.reset();
   }
 } // namespace IKan
