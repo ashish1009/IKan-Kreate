2cf57ebd4c8f15143daf40b1ddaf630f7a92ddaf
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-30 15:40:29 +0530

    Changing Core dir path to std::filesystem

--- IKan/Implementation/Core/EngineDirectoryManager.cpp
+++ IKan/Implementation/Core/EngineDirectoryManager.cpp
@@ -9,35 +9,35 @@
 
 namespace IKan
 {
-  std::string CoreDirectory::GetEngineInstallPath()
+  std::filesystem::path CoreDirectory::GetEngineInstallPath()
   {
     return s_engineBasePath;
   }
   
-  std::string CoreDirectory::GetAssetBasePath()
+  std::filesystem::path CoreDirectory::GetAssetBasePath()
   {
-    return s_engineBasePath + "/Assets/";
+    return s_engineBasePath / "Assets/";
   }
   
-  std::string CoreDirectory::RelativeEnginePath(const std::string &path)
+  std::filesystem::path CoreDirectory::RelativeEnginePath(const std::filesystem::path &path)
   {
-    return s_engineBasePath + path;
+    return s_engineBasePath / path;
   }
   
-  std::string CoreDirectory::RelativeAssetPath(const std::string &assetPath)
+  std::filesystem::path CoreDirectory::RelativeAssetPath(const std::filesystem::path &assetPath)
   {
-    return GetAssetBasePath() + assetPath;
+    return GetAssetBasePath() / assetPath;
   }
   
-  void CoreDirectory::SetPath(const std::string &enginePath)
+  void CoreDirectory::SetPath(const std::filesystem::path &enginePath)
   {
     IK_LOG_INFO(LogModule::IKan, "Engine is Instal path is {0}", Utils::FileSystem::Absolute(enginePath.c_str()));
     
     bool exist = Utils::FileSystem::Exists(enginePath);
-    bool assets = Utils::FileSystem::Exists(enginePath + "/Assets");
-    bool interface = Utils::FileSystem::Exists(enginePath + "/Interface");
-    bool implementation = Utils::FileSystem::Exists(enginePath + "/Implementation");
-    bool vendors = Utils::FileSystem::Exists(enginePath + "/Vendors");
+    bool assets = Utils::FileSystem::Exists(enginePath / "Assets");
+    bool interface = Utils::FileSystem::Exists(enginePath / "Interface");
+    bool implementation = Utils::FileSystem::Exists(enginePath / "Implementation");
+    bool vendors = Utils::FileSystem::Exists(enginePath / "Vendors");
     
     IK_ASSERT(exist and assets and interface and implementation and vendors, "Invalid Engine Directory");
     s_engineBasePath = enginePath;
--- IKan/Implementation/Core/EngineDirectoryManager.hpp
+++ IKan/Implementation/Core/EngineDirectoryManager.hpp
@@ -22,26 +22,26 @@ namespace IKan
   public:
     /// This function returns the core engine base path
     /// - Important: To be setup via application specificaion
-    static std::string GetEngineInstallPath();
+    static std::filesystem::path GetEngineInstallPath();
     /// This function returns the Core Asset Base path
     /// - Important: Returned path is relative to .exce and upto the folder name of 'core_assets'
-    static std::string GetAssetBasePath();
+    static std::filesystem::path GetAssetBasePath();
     
     /// This function returns the path relative to the executable form the path relative to engine
     /// - Parameter path: Path relative to workspace folder:
-    static std::string RelativeEnginePath(const std::string& path);
+    static std::filesystem::path RelativeEnginePath(const std::filesystem::path& path);
     /// This function returns the path relative to the executable form the path relative to Core asset path
     /// - Parameter asset_path: Aasset path relative to core_asset folder:
-    static std::string RelativeAssetPath(const std::string& assetPath);
+    static std::filesystem::path RelativeAssetPath(const std::filesystem::path& assetPath);
     
     MAKE_PURE_STATIC(CoreDirectory);
     friend class Application;
 
   private:
     // Private APIs --------------------------------------------------------------------------------------------------
-    static void SetPath(const std::string& enginePath);
+    static void SetPath(const std::filesystem::path& enginePath);
     
     // Direcotry path of IKan Engine relative to Executable ----------------------------------------------------------
-    inline static std::string s_engineBasePath;
+    inline static std::filesystem::path s_engineBasePath;
   };  
 } // namespace IKan
