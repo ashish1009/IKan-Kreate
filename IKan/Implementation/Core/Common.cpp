8ac68c58a50a3caf8c49e34253de97b7a86a83f1
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-07-31 16:06:34 +0530

    Release 7.0.2.0
    ImGui Theme APIs

--- IKan/Implementation/Core/Common.cpp
+++ IKan/Implementation/Core/Common.cpp
@@ -11,7 +11,7 @@
 
 /// Version number of IKan Engine
 ///- Note: To be updated manually
-std::string IKanVersion = "v7.0.3.0";
+std::string IKanVersion = "v7.0.2.0";
 
 void InitializeEngine(const std::string logDirectoryPath)
 {    
