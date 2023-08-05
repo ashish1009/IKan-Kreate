6890e5267985c31b110ad0f3a7ade2425a6cf7c3
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-08-05 12:06:05 +0530

    Creating Base class of Content Browser Items

--- Kreator/Kreator/Panles/ContentBrowserItem.cpp
+++ Kreator/Kreator/Panles/ContentBrowserItem.cpp
@@ -6,9 +6,12 @@
 //
 
 #include "ContentBrowserItem.hpp"
+#include "ContentBrowserPanel.hpp"
 
 namespace Kreator
-{  
+{
+  static char s_renameBuffer[MAX_INPUT_BUFFER_LENGTH];
+
   void CBItemActionResult::Set(ContentBrowserAction flag, bool value)
   {
     if (value)
@@ -24,5 +27,111 @@ namespace Kreator
   bool CBItemActionResult::IsSet(ContentBrowserAction flag) const
   {
     return (uint16_t)flag & Field;
+  }
+  
+  
+  ContentBrowserItem::ContentBrowserItem(ItemType type, AssetHandle id, const std::string& name,
+                                         const Ref<Texture>& icon)
+  : m_type(type), m_ID(id), m_name(name), m_icon(icon)
+  {
+    
+  }
+  
+  void ContentBrowserItem::OnRenderBegin()
+  {
+  }
+  
+  CBItemActionResult ContentBrowserItem::OnRender()
+  {
+    CBItemActionResult result;
+
+    return result;
+  }
+  
+  void ContentBrowserItem::OnRenderEnd()
+  {
+  }
+  
+  void ContentBrowserItem::StartRenaming()
+  {
+    if (m_isRenaming)
+    {
+      return;
+    }
+    
+    memset(s_renameBuffer, 0, MAX_INPUT_BUFFER_LENGTH);
+    memcpy(s_renameBuffer, m_name.c_str(), m_name.size());
+    m_isRenaming = true;
+  }
+  
+  void ContentBrowserItem::StopRenaming()
+  {
+    m_isRenaming = false;
+    memset(s_renameBuffer, 0, MAX_INPUT_BUFFER_LENGTH);
+  }
+  
+  void ContentBrowserItem::SetSelected(bool value)
+  {
+    m_isSelected = value;
+  }
+  
+  void ContentBrowserItem::Rename(const std::string& newName)
+  {
+    OnRenamed(newName);
+  }
+  
+  AssetHandle ContentBrowserItem::GetID() const
+  {
+    return m_ID;
+  }
+  ContentBrowserItem::ItemType ContentBrowserItem::GetType() const
+  {
+    return m_type;
+  }
+  const std::string& ContentBrowserItem::GetName() const
+  {
+    return m_name;
+  }
+  const Ref<Texture>& ContentBrowserItem::GetIcon() const
+  {
+    return m_icon;
+  }
+  bool ContentBrowserItem::IsSelected() const
+  {
+    return m_isSelected;
+  }
+  bool ContentBrowserItem::IsRenaming() const
+  {
+    return m_isRenaming;
+  }
+  
+  void ContentBrowserItem::OnRenamed(const std::string& newName)
+  {
+    m_name = newName;
+  }
+  
+  void ContentBrowserItem::OnContextMenuOpen(CBItemActionResult& actionResult)
+  {
+    if (ImGui::MenuItem("Reload"))
+    {
+      actionResult.Set(ContentBrowserAction::Reload, true);
+    }
+    
+    if (ImGui::MenuItem("Rename"))
+    {
+      actionResult.Set(ContentBrowserAction::StartRenaming, true);
+    }
+    
+    if (ImGui::MenuItem("Copy"))
+    {
+      actionResult.Set(ContentBrowserAction::Copy, true);
+    }
+    
+    if (ImGui::MenuItem("Delete"))
+    {
+      actionResult.Set(ContentBrowserAction::OpenDeleteDialogue, true);
+    }
+    
+    RenderCustomContextItems();
   }
 } // namespace Kreator
