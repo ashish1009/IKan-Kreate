4f339afe169612ad5f3e385079e6974642311fda
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-08-05 12:27:27 +0530

    Updating content browser project

--- Kreator/Kreator/Panles/ContentBrowserPanel.cpp
+++ Kreator/Kreator/Panles/ContentBrowserPanel.cpp
@@ -183,34 +183,175 @@ namespace Kreator
   
   void ContentBrowserPanel::OnProjectChanged(const Ref<Project> &project)
   {
-    IK_ASSERT(false);
-
+    if (!project)
+    {
+      return;
+    }
+    
+    // Clear all the data if project is changed
+    m_directories.clear();
+    m_currentItems.Clear();
+    m_baseDirectory = nullptr;
+    m_currentDirectory = nullptr;
+    m_nextDirectory = nullptr;
+    m_previousDirectory = nullptr;
+    m_selectionStack.Clear();
+    m_breadCrumbData.clear();
+    
+    // Updater the project
+    m_project = project;
+    
+    AssetHandle baseDirectoryHandle = ProcessDirectory(project->GetAssetDirectory().string(), nullptr);
+    m_baseDirectory = m_directories[baseDirectoryHandle];
+    ChangeDirectory(m_baseDirectory);
   }
   
   AssetHandle ContentBrowserPanel::ProcessDirectory(const std::filesystem::path &directoryPath, const Ref<DirectoryInfo> &parent)
   {
-    IK_ASSERT(false);
-
+    // Get Directory from pre processed directories
+    const auto& directory = GetDirectory(directoryPath);
+    
+    // If directory already preprocessed then do not process again
+    if (directory)
+    {
+      return directory->handle;
+    }
+    
+    // Create directory info instance for storing in directories
+    Ref<DirectoryInfo> directoryInfo = CreateRef<DirectoryInfo>();
+    directoryInfo->handle = AssetHandle();
+    directoryInfo->parent = parent;
+    
+    if (directoryPath == m_project->GetAssetDirectory())
+    {
+      directoryInfo->filePath = "";
+    }
+    else
+    {
+      directoryInfo->filePath = std::filesystem::relative(directoryPath, m_project->GetAssetDirectory());
+    }
+    
+    // Add Subdirectories or assets recursivally
+    for (auto entry : std::filesystem::directory_iterator(directoryPath))
+    {
+      if (entry.is_directory())
+      {
+        AssetHandle subdirHandle = ProcessDirectory(entry.path(), directoryInfo);
+        directoryInfo->subDirectories[subdirHandle] = m_directories[subdirHandle];
+        continue;
+      }
+      
+      auto metadata = AssetManager::GetMetadata(std::filesystem::relative(entry.path(), m_project->GetAssetDirectory()));
+      if (!metadata.IsValid())
+      {
+        AssetType type = AssetManager::GetAssetTypeFromPath(entry.path());
+        if (type == AssetType::None)
+        {
+          continue;
+        }
+        metadata.handle = AssetManager::ImportAsset(entry.path());
+      }
+      
+      // Failed to import
+      if (!metadata.IsValid())
+      {
+        continue;
+      }
+      
+      directoryInfo->assets.push_back(metadata.handle);
+    }
+    
+    // Store the directory info in memory to reuse them
+    m_directories[directoryInfo->handle] = directoryInfo;
+    return directoryInfo->handle;
   }
   
   Ref<DirectoryInfo> ContentBrowserPanel::GetDirectory(const std::filesystem::path& filepath) const
   {
-    IK_ASSERT(false);
+    if (filepath.string() == "" || filepath.string() == ".")
+    {
+      return m_baseDirectory;
+    }
+    
+    for (const auto&[handle, directory] : m_directories)
+    {
+      if (directory->filePath == filepath)
+      {
+        return directory;
+      }
+    }
+    return nullptr;
   }
   
   void ContentBrowserPanel::ChangeDirectory(Ref<DirectoryInfo> &directory)
   {
-    IK_ASSERT(false);
+    // Check if directory is valid
+    if (!directory)
+    {
+      return;
+    }
+    
+    m_updateNavigationPath = true;
+    
+    // Clear the current item list and update with new subdirectories
+    m_currentItems.items.clear();
+    for (auto&[subdirHandle, subdir] : directory->subDirectories)
+    {
+      // Create Item list for directory
+      m_currentItems.items.push_back(CreateRef<ContentBrowserDirectory>(subdir, m_folderIcon));
+    }
+    
+    // Update the Assets in Current item lists
+    for (auto assetHandle : directory->assets)
+    {
+      AssetMetadata metadata = AssetManager::GetMetadata(assetHandle);
+      if (metadata.IsValid())
+      {
+        // Set the asset texture
+        Ref<Texture> assetTexture = m_fileTex;
+        if (m_assetIconMap.find(metadata.filePath.extension().string()) != m_assetIconMap.end())
+        {
+          assetTexture = m_assetIconMap[metadata.filePath.extension().string()];
+        }
+        
+        // Create Item list for asset
+        m_currentItems.items.push_back(CreateRef<ContentBrowserAsset>(metadata, assetTexture));
+      }
+    }
+    
+    SortItemList();
+    
+    m_previousDirectory = directory;
+    m_currentDirectory = directory;
+    
+    ClearSelections();
   }
   
   void ContentBrowserPanel::SortItemList()
   {
-    IK_ASSERT(false);
+    std::sort(m_currentItems.begin(), m_currentItems.end(), [](const Ref<ContentBrowserItem>& item1, const Ref<ContentBrowserItem>& item2)
+              {
+      if (item1->GetType() == item2->GetType())
+      {
+        return Utils::String::ToLower(item1->GetName()) < Utils::String::ToLower(item2->GetName());
+      }
+      return (uint16_t)item1->GetType() < (uint16_t)item2->GetType();
+    });
   }
   
   void ContentBrowserPanel::ClearSelections()
   {
-    IK_ASSERT(false);
+    for (auto& item : m_currentItems)
+    {
+      item->SetSelected(false);
+      
+      if (item->IsRenaming())
+      {
+        item->StopRenaming();
+      }
+    }
+    
+    m_selectionStack.Clear();
   }
   
   void ContentBrowserPanel::UpdateDropArea(const Ref<DirectoryInfo>& target)
