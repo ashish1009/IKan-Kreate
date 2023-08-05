7dcb478c9783eed94cc1ad8967c90838b02f00d7
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-08-05 12:03:29 +0530

    Adding content browser Action list

--- Kreator/Kreator/Panles/ContentBrowserItem.hpp
+++ Kreator/Kreator/Panles/ContentBrowserItem.hpp
@@ -18,4 +18,25 @@ namespace Kreator
     std::vector<AssetHandle> assets;
     std::map<AssetHandle, Ref<DirectoryInfo>> subDirectories;
   };
+  
+  enum class ContentBrowserAction
+  {
+    None                = 0,
+    Refresh             = BIT(0),
+    ClearSelections     = BIT(1),
+    Selected            = BIT(2),
+    Hovered             = BIT(3),
+    Renamed             = BIT(4),
+    NavigateToThis      = BIT(5),
+    OpenDeleteDialogue  = BIT(6),
+    SelectToHere        = BIT(7),
+    Moved               = BIT(8),
+    ShowInExplorer      = BIT(9),
+    OpenExternal        = BIT(10),
+    Reload              = BIT(11),
+    Copy                = BIT(12),
+    Duplicate           = BIT(13),
+    StartRenaming       = BIT(14)
+  };
+
 } // namespace Kreator
