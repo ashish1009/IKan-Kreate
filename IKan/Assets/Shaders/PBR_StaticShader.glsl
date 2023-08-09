2f5077dae3d86b16d8b01f437817b12ca7d0fcae
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-08-09 14:41:33 +0530

    Adding PBR Static Vertex Shader

---
+++ IKan/Assets/Shaders/PBR_StaticShader.glsl
@@ -0,0 +1,72 @@
+//
+//  PBR_StaticShader.glsl
+//  ikan
+//
+//  Created by iKan on 09/08/23.
+//
+
+// vertex Shader
+#type vertex
+
+#version 330 core
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec3 a_Normal;
+layout (location = 2) in vec3 a_Tangent;
+layout (location = 3) in vec3 a_Binormal;
+layout (location = 4) in vec2 a_TexCoord;
+
+out VS_OUT
+{
+  vec3 WorldPosition;
+  vec3 Normal;
+  vec2 TexCoord;
+  mat3 WorldNormals;
+  mat3 WorldTransform;
+  vec3 Binormal;
+  float ObjectID;
+} vs_Output;
+
+uniform mat4 u_ViewProjection;
+uniform mat4 u_Transform;
+
+
+/// Vertex Main Function
+void main()
+{
+  vs_Output.WorldPosition = vec3(u_Transform * vec4(a_Position, 1.0));
+  vs_Output.Normal = mat3(u_Transform) * a_Normal;
+  vs_Output.TexCoord = vec2(a_TexCoord.x, 1.0 - a_TexCoord.y);
+  vs_Output.WorldNormals = mat3(u_Transform) * mat3(a_Tangent, a_Binormal, a_Normal);
+  vs_Output.WorldTransform = mat3(u_Transform);
+  vs_Output.Binormal = a_Binormal;
+  vs_Output.ObjectID  = a_ObjectID;
+  
+  vs_Output.TexCoord = vs_Output.TexCoord;
+  
+  gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+}
+
+// Fragment Shader
+#type fragment
+
+#version 330 core
+layout (location = 0) out vec4 o_Color;
+layout (location = 1) out int  o_IDBuffer;
+
+in VS_OUT
+{
+  vec3 WorldPosition;
+  vec3 Normal;
+  vec2 TexCoord;
+  mat3 WorldNormals;
+  mat3 WorldTransform;
+  vec3 Binormal;
+  float ObjectID;
+} vs_Input;
+
+/// Fragment Main Function
+void main()
+{
+  o_Color = vec4(1.0f);
+  o_IDBuffer = int(vs_Input.ObjectID);
+}
