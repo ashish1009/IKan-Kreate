67c775ed98c48a2b18a863a6f8dddbedc135f4d3
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-08-31 14:44:42 +0530

    AddinG Editor Console Message

--- IKan/Implementation/Editor/EditorConsoleMessage.cpp
+++ IKan/Implementation/Editor/EditorConsoleMessage.cpp
@@ -5,4 +5,37 @@
 //  Created by Ashish . on 04/08/23.
 //
 
+#include "EditorConsoleMessage.hpp"
+
+namespace IKan
+{
+  ConsoleMessage::ConsoleMessage()
+  : m_messageID(0), m_message(""), m_count(0), m_category(Category::None)
+  {
+    
+  }
+  
+  ConsoleMessage::ConsoleMessage(const std::string& message, Category category)
+  : m_messageID(std::hash<std::string>()(message)), m_message(message), m_count(1), m_category(category)
+  {
+    
+  }
+  
+  uint64_t ConsoleMessage::GetMessageID() const
+  {
+    return m_messageID;
+  }
+  const std::string& ConsoleMessage::GetMessage() const
+  {
+    return m_message;
+  }
+  uint32_t ConsoleMessage::GetCount() const
+  {
+    return m_count;
+  }
+  ConsoleMessage::Category ConsoleMessage::GetCategory() const
+  {
+    return m_category;
+  }
+} // namespace IKan
 
