5410e1ac3b256a1711c6181cc733a9dad4b12333
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-09-04 12:00:07 +0530

    Creating Asset Serializer class

--- IKan/Implementation/Asset/AssetSerializer.cpp
+++ IKan/Implementation/Asset/AssetSerializer.cpp
@@ -6,3 +6,35 @@
 //
 
 #include "AssetSerializer.hpp"
+#include "Renderer/UI/Font.hpp"
+#include "Renderer/Graphics/Texture.hpp"
+#include "Scene/Scene.hpp"
+#include "Asset/AssetManager.hpp"
+#include "Renderer/Mesh.hpp"
+
+namespace IKan
+{
+  bool ImageSerializer::TryLoadData(const AssetMetadata &metadata, Ref<Asset> &asset) const
+  {
+    IK_ASSERT(false);
+    return true;
+  }
+  
+  bool SceneAssetSerializer::TryLoadData(const AssetMetadata &metadata, Ref<Asset> &asset) const
+  {
+    IK_ASSERT(false);
+    return true;
+  }
+  
+  bool FontSerializer::TryLoadData(const AssetMetadata &metadata, Ref<Asset> &asset) const
+  {
+    IK_ASSERT(false);
+    return true;
+  }
+  
+  bool MeshSourceSerializer::TryLoadData(const AssetMetadata &metadata, Ref<Asset> &asset) const
+  {
+    IK_ASSERT(false);
+    return true;
+  }  
+} // namespace IKan
--- IKan/Interface/Asset/AssetSerializer.hpp
+++ IKan/Interface/Asset/AssetSerializer.hpp
@@ -7,3 +7,62 @@
 
 #pragma once
 
+#include "Asset/AssetMetadata.hpp"
+
+namespace IKan
+{
+  /// This class serialises the asset
+  class AssetSerializer
+  {
+  public:
+    virtual ~AssetSerializer() = default;
+    
+    /// This function serialise the asset
+    /// - Parameters:
+    ///   - metadata: Asset Metadata
+    ///   - asset: Asset Instance
+    virtual void Serialize(const AssetMetadata& metadata, const Ref<Asset>& asset) const = 0;
+    /// This function try to load the asset
+    /// - Parameters:
+    ///   - metadata: Asset meta data
+    ///   - asset: Asset instance
+    virtual bool TryLoadData(const AssetMetadata& metadata, Ref<Asset>& asset) const = 0;
+  };
+  
+  class ImageSerializer : public AssetSerializer
+  {
+  public:
+    /// @see: AssetSerializer
+    virtual void Serialize(const AssetMetadata& metadata, const Ref<Asset>& asset) const override{ IK_ASSERT(false);}
+    /// @see: AssetSerializer
+    virtual bool TryLoadData(const AssetMetadata& metadata, Ref<Asset>& asset) const override;
+  };
+  
+  class FontSerializer : public AssetSerializer
+  {
+  public:
+    /// @see: AssetSerializer
+    virtual void Serialize(const AssetMetadata& metadata, const Ref<Asset>& asset) const override {IK_ASSERT(false);}
+    /// @see: AssetSerializer
+    virtual bool TryLoadData(const AssetMetadata& metadata, Ref<Asset>& asset) const override;
+  };
+  
+  class SceneAssetSerializer : public AssetSerializer
+  {
+  public:
+    /// @see: AssetSerializer
+    virtual void Serialize(const AssetMetadata& metadata, const Ref<Asset>& asset) const override{ IK_ASSERT(false);}
+    /// @see: AssetSerializer
+    virtual bool TryLoadData(const AssetMetadata& metadata, Ref<Asset>& asset) const override;
+  };
+  
+  class MeshSourceSerializer : public AssetSerializer
+  {
+  public:
+    /// @see: AssetSerializer
+    virtual void Serialize(const AssetMetadata& metadata, const Ref<Asset>& asset) const override{ IK_ASSERT(false);}
+    /// @see: AssetSerializer
+    virtual bool TryLoadData(const AssetMetadata& metadata, Ref<Asset>& asset) const override;
+  };
+  
+} // namespace IKan
