2f3c9a203b7c7763a21cc701cc11f2b98be25841
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-09-04 15:29:50 +0530

    Adding Scene Serializer basic

--- IKan/Implementation/Scene/SceneSerializer.cpp
+++ IKan/Implementation/Scene/SceneSerializer.cpp
@@ -20,10 +20,47 @@ namespace IKan {
   void SceneSerializer::Serialize(const std::string& filepath)
   {
     IK_LOG_TRACE(LogModule::SceneSerializer, "Serializing the Scene {0}", Utils::String::GetFileNameFromPath(filepath));
+    
+    m_scene->SetName(Utils::String::GetFileNameFromPath(filepath));
+    
+    YAML::Emitter out;
+    out << YAML::BeginMap;
+    out << YAML::Key << "Scene";
+    out << YAML::Value << m_scene->GetName();
+
+    out << YAML::Key << "Entities";
+    out << YAML::Value << YAML::BeginSeq;
+
+    out << YAML::EndSeq;
+    out << YAML::EndMap;
+    
+    std::ofstream fout(filepath);
+    fout << out.c_str();
   }
   
   bool SceneSerializer::Deserialize(const std::string& filepath)
   {
+    std::ifstream stream(filepath);
+    IK_ASSERT(stream);
+    
+    std::stringstream strStream;
+    strStream << stream.rdbuf();
+    
+    YAML::Node data = YAML::Load(strStream.str());
+    if (!data["Scene"])
+      return false;
+    
+    std::string sceneName = data["Scene"].as<std::string>();
+    IK_LOG_TRACE(LogModule::SceneSerializer, "Deserializing scene '{0}'", sceneName);
+    
+    if (sceneName == "UntitledScene")
+    {
+      std::filesystem::path path = filepath;
+      sceneName = path.stem().string();
+    }
+    
+    m_scene->SetName(sceneName);
+
     return true;
   }  
 } // namespace IKan
