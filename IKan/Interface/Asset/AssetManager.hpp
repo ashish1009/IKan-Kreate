2ad10da4666891782dc687db73038ff61c67c686
Ashish <22274092+ashish1009@users.noreply.github.com>
2023-09-11 11:15:24 +0530

    Rename Crete Asset with File again
    CreateAssetWithFile -> CreateAsset

--- IKan/Interface/Asset/AssetManager.hpp
+++ IKan/Interface/Asset/AssetManager.hpp
@@ -107,7 +107,7 @@ namespace IKan
     ///   TAsset - Type of asset
     ///   TArgs  - Dynamic arguments for asset creation
     template<typename TAsset, typename... TArgs>
-    static AssetHandle CreateAssetWithFile(const std::string& fileName, TArgs&&... args)
+    static AssetHandle CreateAsset(const std::string& fileName, TArgs&&... args)
     {
       static_assert(std::is_base_of<Asset, TAsset>::value,
                     "CreateMemoryOnlyAsset only works for types derived from Asset");
