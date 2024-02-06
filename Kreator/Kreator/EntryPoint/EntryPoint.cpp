92e44eee9b1a45c942d575dd4c9ca99fab38bc7b
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-06 15:47:08 +0530

    Creating Project for executable Kreator Application

---
+++ Kreator/Kreator/EntryPoint/EntryPoint.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Kreator
+//
+//  Created by Ashish . on 06/02/24.
+//
+
+#include <iostream>
+
+/// This is the Entry point of the Kreator
+/// - Parameters:
+///   - argc: Number of arguments passed from binary
+///   - argv: Arguments ...
+int main(int argc, const char * argv[])
+{
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
