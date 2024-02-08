af0dfcc1f6e25abc97044f56adc7b2020b16486a
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 17:33:58 +0530

    Creating Frame Buffer interface class

--- IKan/Interface/Renderer/Graphics/FrameBuffer.hpp
+++ IKan/Interface/Renderer/Graphics/FrameBuffer.hpp
@@ -7,7 +7,44 @@
 
 #pragma once
 
+#include "Renderer/Renderer.hpp"
+#include "Renderer/Graphics/Factories.hpp"
+#include "Renderer/Graphics/Specifications.hpp"
+
 namespace IKan
 {
+  class Texture2D;
   
+  /// Interface class for storing Framebuffer
+  class FrameBuffer
+  {
+  public:
+    /// Default virtual destructor
+    virtual ~FrameBuffer() = default;
+    
+    /// This function binds the current FrameBuffer to the renderer
+    virtual void Bind() const = 0;
+    /// This function unbinds the current FrameBuffer from the renderer
+    virtual void Unbind() const = 0;
+    /// This function resizes the Framebuffer
+    /// - Parameters:
+    ///   - width: New width of FB
+    ///   - height: New height of FB
+    virtual void Resize(uint32_t width, uint32_t height) = 0;
+    
+    /// This funciton updates the specification color
+    /// - Parameter color: color
+    virtual void UpdateSpecificationColor(const glm::vec4& color) = 0;
+    
+    /// This function returns the Frame buffer specification
+    virtual const FrameBufferSpecification& GetSpecification() const = 0;
+    /// This function returns the Renderer ID
+    virtual RendererID GetRendererId() const = 0;
+    /// This function returns the Depth Attachment ID
+    virtual Ref<Texture> GetDepthAttachment() const = 0;
+    /// This function returns the Color Attachment iDs
+    virtual const std::vector<Ref<Texture>>& GetColorAttachments() const = 0;
+    /// This function returns the pixel id (RED_ID) index stored in fremebuffer
+    virtual uint32_t GetPixelIdIndex() const = 0;
+  };
 } // namespace IKan
