3faf076269dd1dc8d63e530637a35a285bc6dda6
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 13:45:09 +0530

    Creating Open GL Implementation class for Index buffer

--- IKan/Implementation/Platform/OpenGL/OpenGLRendererBuffer.cpp
+++ IKan/Implementation/Platform/OpenGL/OpenGLRendererBuffer.cpp
@@ -78,4 +78,56 @@ namespace IKan
     return m_size;
   }
 
+  OpenGLIndexBuffer::OpenGLIndexBuffer(void *data, uint32_t size)
+  : m_size(size)
+  {
+    glGenBuffers(1, &m_rendererID);
+    RendererStatistics::Get().indexBufferSize += m_size;
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_size, data, GL_STATIC_DRAW);
+    
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "Creating Open GL Index Buffer ");
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Renderer ID            {0}", m_rendererID);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Size of single Indices {0} B", SizeOfSingleIndices);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Size                   {0} B", m_size);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Total Size Used        {0} B", RendererStatistics::Get().indexBufferSize);
+  }
+  
+  OpenGLIndexBuffer::~OpenGLIndexBuffer()
+  {
+    RendererStatistics::Get().indexBufferSize -= m_size;
+    glDeleteBuffers(1, &m_rendererID);
+    
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "Destroying Open GL Index Buffer ");
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Renderer ID            {0}", m_rendererID);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Size of single Indices {0} B", SizeOfSingleIndices);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Size                   {0} B", m_size);
+    IK_LOG_DEBUG(LogModule::IndexBuffer, "  Total Size Left        {0} B", RendererStatistics::Get().indexBufferSize);
+  }
+  
+  void OpenGLIndexBuffer::Bind() const
+  {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererID);
+  }
+  
+  void OpenGLIndexBuffer::Unbind() const
+  {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  }
+  
+  RendererID OpenGLIndexBuffer::GetRendererID() const
+  {
+    return m_rendererID;
+  }
+  
+  uint32_t OpenGLIndexBuffer::GetSize() const
+  {
+    return m_size;
+  }
+  
+  uint32_t OpenGLIndexBuffer::GetCount() const
+  {
+    return m_size / SizeOfSingleIndices;
+  }
 } // namespace IKan
--- IKan/Implementation/Platform/OpenGL/OpenGLRendererBuffer.hpp
+++ IKan/Implementation/Platform/OpenGL/OpenGLRendererBuffer.hpp
@@ -51,4 +51,34 @@ namespace IKan
     uint32_t m_size {0};
   };
 
+  /// This is the implementation class to create Index Buffer
+  class OpenGLIndexBuffer : public IndexBuffer
+  {
+  public:
+    /// This Constructor create the Index Buffer with Indices data and its size
+    /// - Parameters:
+    ///   - data: data to be filled in index buffer
+    ///   - size: size of data in index buffer
+    OpenGLIndexBuffer(void *data, uint32_t size);
+    /// This is the default destructor for Open GL Index buffer
+    ~OpenGLIndexBuffer();
+    
+    /// This function binds the Vertex Buffer before rendering
+    void Bind() const override;
+    /// This function unbinds the Vertex Buffer after rendering
+    void Unbind() const override;
+    
+    /// This function returns the Number of Indices used by this Index Buffer
+    uint32_t GetCount() const override;
+    /// This function returns the size of Index Buffer in GPU
+    uint32_t GetSize() const override;
+    /// This function return the renderer ID of Index Buffer
+    RendererID GetRendererID() const override;
+    
+    DELETE_COPY_MOVE_CONSTRUCTORS(OpenGLIndexBuffer);
+    
+  private:
+    RendererID m_rendererID;
+    uint32_t m_size {0};
+  };
 } // namespace IKan
