3e19c5880504b2b23d419eea53b646c25c5c246f
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 12:27:14 +0530

    Creating Shader Uniform declaration Interface class

--- IKan/Interface/Renderer/Graphics/ShaderUniforms.hpp
+++ IKan/Interface/Renderer/Graphics/ShaderUniforms.hpp
@@ -9,5 +9,40 @@
 
 namespace IKan
 {
+  /// This enum stores the type of shader
+  enum class ShaderDomain
+  {
+    None = 0,
+    Vertex = 1,
+    Fragment = 2,
+    Geometry = 3
+  };
   
+  /// This is the interface class to store shader uniform elements (like int, float, vec ....)
+  class ShaderUniformDeclaration
+  {
+  public:
+    /// Virtual Default destcutor for Shader Uniform Declaration
+    virtual ~ShaderUniformDeclaration() = default;
+    
+    /// This function returns the name of field
+    virtual const std::string& GetName() const = 0;
+    /// This functionr returns the size of field
+    virtual uint32_t GetSize() const = 0;
+    /// This function returns the count of field
+    virtual uint32_t GetCount() const = 0;
+    /// This function returns the offset of field
+    virtual uint32_t GetOffset() const = 0;
+    
+    /// This function returns the domain of shader
+    virtual ShaderDomain GetDomain() const = 0;
+    
+    friend class Shader;
+    friend class OpenGLShader;
+    friend class ShaderStruct;
+    
+  protected:
+    /// This function update the offset of the field
+    virtual void SetOffset(uint32_t offset) = 0;
+  };
 } // namespace IKan
