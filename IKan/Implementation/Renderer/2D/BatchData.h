706e2ffeb2a233e20edea6cb0c7064380f7c9895
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 18:53:28 +0530

    Creating Shape 2D Common Batch structure

--- IKan/Implementation/Renderer/2D/BatchData.h
+++ IKan/Implementation/Renderer/2D/BatchData.h
@@ -36,6 +36,7 @@ namespace IKan
     
     void Destroy()
     {
+      IK_PROFILE();
       maxElement = 0;
       maxVertices = 0;
       
@@ -46,4 +47,71 @@ namespace IKan
     }
   };
 
+  struct Shape2DCommonData : CommonBatchData
+  {
+    /// Single vertex for a Quad or Circle
+    struct CommonVertex
+    {
+      glm::vec3 position;
+      glm::vec4 color;
+      glm::vec2 textureCoords;
+      
+      float textureIndex;
+      float tilingFactor;
+      
+      int32_t pixelID;
+    };
+    
+    // Constants
+    static constexpr uint32_t VertexForSingleElement = 4;
+    static constexpr uint32_t IndicesForSingleElement = 6;
+    
+    /// Store the Vertex and Indices size
+    uint32_t maxIndices = 0;
+    /// Count of Indices to be renderer in Single Batch
+    uint32_t indexCount = 0;
+    
+    /// Texture Slot index sent to Shader to render a specific Texture from slots
+    /// NOTE: Slot 0 is reserved for white texture (No Image only color)
+    uint32_t textureSlotIndex = 1; // 0 = white texture
+    
+    /// Stores all the 16 Texture in array so that there is no need to load texture each frame
+    /// NOTE: Load only if new texture is added or older replaced with new
+    std::array<Ref<Texture>, MaxTextureSlotsInShader> textureSlots;
+    
+    /// Basic vertex of quad (Vertex of circle is taken as Quad only)
+    glm::vec4 vertexBasePosition[4];
+    
+    // Member Functions
+    void InitializeShapeData(uint32_t maxElement)
+    {
+      IK_PROFILE();
+      CommonBatchData::maxElement = maxElement;
+      maxVertices = maxElement * VertexForSingleElement;
+      maxIndices = CommonBatchData::maxElement * IndicesForSingleElement;
+      
+      // Setting basic Vertex point of quad
+      vertexBasePosition[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
+      vertexBasePosition[1] = {  0.5f, -0.5f, 0.0f, 1.0f };
+      vertexBasePosition[2] = {  0.5f,  0.5f, 0.0f, 1.0f };
+      vertexBasePosition[3] = { -0.5f,  0.5f, 0.0f, 1.0f };
+      
+      // Creating white texture for colorful (default white) quads witout any image file
+      if (!textureSlots[0])
+      {
+        textureSlots[0] = TextureFactory::Create(0xffffffff);
+      }
+    }
+    
+    void Destroy()
+    {
+      IK_PROFILE();
+      maxIndices = 0;
+      for (uint32_t i = 0; i < MaxTextureSlotsInShader; i++)
+      {
+        textureSlots[i].reset();
+      }
+      CommonBatchData::Destroy();
+    }
+  };
 } // namespace IKan
