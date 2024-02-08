62262e0e10316e62e333a3bdba691685747f6e38
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 12:13:30 +0530

    Starting Shader Buffer Uniform parsing

---
+++ IKan/Implementation/Platform/OpenGL/ShaderUniforms.cpp
@@ -0,0 +1,13 @@
+//
+//  ShaderUniforms.cpp
+//  IKan
+//
+//  Created by Ashish . on 08/02/24.
+//
+
+#include "ShaderUniforms.hpp"
+
+namespace IKan
+{
+  
+} // namespace IKan
---
+++ IKan/Interface/Renderer/Graphics/ShaderUniforms.hpp
@@ -0,0 +1,13 @@
+//
+//  ShaderUniforms.hpp
+//  IKan
+//
+//  Created by Ashish . on 08/02/24.
+//
+
+#pragma once
+
+namespace IKan
+{
+  
+} // namespace IKan
