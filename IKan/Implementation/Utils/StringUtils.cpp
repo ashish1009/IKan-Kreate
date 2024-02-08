77844b898570bfbfa576f9f23958532db103388b
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-08 12:38:20 +0530

    Adding String helper APIs

--- IKan/Implementation/Utils/StringUtils.cpp
+++ IKan/Implementation/Utils/StringUtils.cpp
@@ -40,4 +40,116 @@ namespace IKan::Utils
     }
     return result;
   }
+  
+  const char* String::FindToken(const char* str, const std::string& token)
+  {
+    const char* t = str;
+    while ((t = strstr(t, token.c_str()))) {
+      // Check if left character of token is empty
+      // using '/' as indicator of first character of line
+      bool left = str == t or isspace(t[-1]) or t[-1] == '/';
+      
+      // Check if right character of token is empty
+      bool right = !t[token.size()] or isspace(t[token.size()]);
+      
+      // Checking token is whole word
+      if (left and right)
+      {
+        return t;
+      }
+      
+      t += token.size();
+    }
+    return nullptr;
+  }
+  
+  std::string String::GetBlock(const char* str, const char** outPosition) {
+    // get the end block brace of the string code
+    const char* end = strstr(str, "}");
+    
+    // if not found any end bkock statement "}" then return the same string
+    if (!end)
+    {
+      return str;
+    }
+    
+    // Update the out_position pointer after the end block
+    // offset the shader string to the end of current strcut
+    if (outPosition)
+    {
+      *outPosition = end;
+    }
+    
+    uint32_t length = (uint32_t)(end - str + 1);
+    // return the substring from start to last "}"
+    return std::string(str, length);
+  }
+  
+  std::string String::GetStatement(const char* str, const char** outPosition)
+  {
+    // get the end block brace of the string code
+    const char* end = strstr(str, ";");
+    
+    // if not found any end bkock statement ";" then return the same string
+    if (!end)
+    {
+      return str;
+    }
+    
+    // Update the out_position pointer after the end block
+    // offset the shader string to the end of current strcut
+    if (outPosition)
+    {
+      *outPosition = end;
+    }
+    
+    uint32_t length = (uint32_t)(end - str + 1);
+    // return the substring from start to last ";"
+    return std::string(str, length);
+  }
+  
+  std::vector<std::string> String::SplitString(const std::string& string, const std::string& delimiters)
+  {
+    size_t start = 0;
+    // Find the first occurance of splitter
+    size_t end = string.find_first_of(delimiters);
+    
+    // Stores the result
+    std::vector<std::string> result;
+    
+    // while end of string
+    while (end <= std::string::npos)
+    {
+      // Store the split block
+      std::string token = string.substr(start, end - start);
+      if (!token.empty())
+      {
+        result.push_back(token);
+      }
+      
+      if (end == std::string::npos)
+      {
+        break;
+      }
+      
+      start = end + 1;
+      end = string.find_first_of(delimiters, start);
+    }
+    return result;
+  }
+  
+  std::vector<std::string> String::SplitString(const std::string& string, const char delimiter)
+  {
+    return SplitString(string, std::string(1, delimiter));
+  }
+  
+  std::vector<std::string> String::Tokenize(const std::string& string)
+  {
+    return SplitString(string, " \t\n");
+  }
+  
+  std::vector<std::string> String::GetLines(const std::string& string)
+  {
+    return SplitString(string, "\n");
+  }
 } // namespace IKan::Utils
