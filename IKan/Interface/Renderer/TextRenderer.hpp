36fd75ae4cb159fa4eb63ea977ead063a1eee131
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-09 11:43:46 +0530

    Begin and End batch for Text Renderer

--- IKan/Interface/Renderer/TextRenderer.hpp
+++ IKan/Interface/Renderer/TextRenderer.hpp
@@ -20,5 +20,20 @@ namespace IKan
     static void Initialize();
     /// This function Shuts down on Text the Batch Renderer. Delete all the allocated Data
     static void Shutdown();
+    
+    /// This function begins the Batch for 2D Rendere (to be called each frame)
+    /// - Parameter camViewProjMat: Camera View projection Matrix
+    static void BeginBatch(const glm::mat4& camViewProjMat);
+    /// This function Ends the current batch by rendering all the vertex
+    static void EndBatch();
+
+    DELETE_ALL_CONSTRUCTORS(TextRenderer);
+    
+  private:
+    /// This function flsh a single batch
+    static void Flush();
+    /// This function moves to next batch in single frame
+    static void NextBatch();
+
   };
 } // namespace IKan
