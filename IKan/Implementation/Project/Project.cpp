83bc655f8e707a42e28db891ce7d5015a4431e3c
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-15 13:01:01 +0530

    Creating Project class

--- IKan/Implementation/Project/Project.cpp
+++ IKan/Implementation/Project/Project.cpp
@@ -6,3 +6,28 @@
 //
 
 #include "Project.hpp"
+
+namespace IKan
+{
+  Project::Project()
+  {
+  }
+
+  void Project::SetActive(Ref<Project> project)
+  {
+  }
+  
+  void Project::CloseActive()
+  {
+  }
+  
+  const ProjectConfig& Project::GetConfig() const
+  {
+    return m_config;
+  }
+
+  Ref<Project> Project::GetActive()
+  {
+    return s_activeProject;
+  }
+} // namespace IKan
--- IKan/Interface/Project/Project.hpp
+++ IKan/Interface/Project/Project.hpp
@@ -32,4 +32,26 @@ namespace IKan
     std::string projectFileName;
     std::filesystem::path projectDirectory;
   };
+  
+  class Project
+  {
+  public:
+    /// This constructor creates the project and creates the directories if needed
+    Project();
+
+    /// This funciton retusns the current project config
+    const ProjectConfig& GetConfig() const;
+
+    /// This function returns the active project
+    static Ref<Project> GetActive();
+    /// This function updates the active project instance
+    /// - Parameter project: Active project
+    static void SetActive(Ref<Project> project);
+    /// This function closes the active project
+    static void CloseActive();
+    
+  private:
+    ProjectConfig m_config;
+    inline static Ref<Project> s_activeProject;
+  };
 } // namespace IKan
