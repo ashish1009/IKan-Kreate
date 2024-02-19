d76df776c41e0895d2cbf3bfd3484f5d3603efed
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-19 13:03:49 +0530

    Adding Asset handle

---
+++ IKan/Implementation/Asset/Asset.cpp
@@ -0,0 +1,8 @@
+//
+//  Asset.cpp
+//  IKan
+//
+//  Created by Ashish . on 19/02/24.
+//
+
+#include "Asset.hpp"
---
+++ IKan/Interface/Asset/Asset.hpp
@@ -0,0 +1,15 @@
+//
+//  Asset.hpp
+//  IKan
+//
+//  Created by Ashish . on 19/02/24.
+//
+
+#pragma once
+
+#include "Core/UUID.hpp"
+
+namespace IKan
+{
+  using AssetHandle = UUID;
+} // namespace IKan
