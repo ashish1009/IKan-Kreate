4091cd47987d649dc05920324350e6d76f43aa04
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-20 10:44:23 +0530

    Creating Asset base class

--- IKan/Implementation/Asset/Asset.cpp
+++ IKan/Implementation/Asset/Asset.cpp
@@ -6,3 +6,42 @@
 //
 
 #include "Asset.hpp"
+
+namespace IKan
+{
+  AssetType Asset::GetStaticType()
+  {
+    return AssetType::Invalid;
+  }
+  
+  bool Asset::IsValid() const
+  {
+    return ((flags & (uint16_t)AssetFlag::Missing) | (flags & (uint16_t)AssetFlag::Invalid)) == 0;
+  }
+  
+  bool Asset::operator==(const Asset& other) const
+  {
+    return handle == other.handle;
+  }
+  
+  bool Asset::operator!=(const Asset& other) const
+  {
+    return !(*this == other);
+  }
+  
+  bool Asset::IsFlagSet(AssetFlag flag) const
+  {
+    return (uint16_t)flag & flags;
+  }
+  void Asset::SetFlag(AssetFlag flag, bool value)
+  {
+    if (value)
+    {
+      flags |= (uint16_t)flag;
+    }
+    else
+    {
+      flags &= ~(uint16_t)flag;
+    }
+  }
+} // namespace IKan
