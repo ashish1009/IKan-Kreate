8b840822bcf5ba8f6df299d5f863fedeb8bc9dbc
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-21 15:13:30 +0530

    Creating mendatory Component

---
+++ IKan/Implementation/Scene/Components.cpp
@@ -0,0 +1,8 @@
+//
+//  Components.cpp
+//  IKan
+//
+//  Created by Ashish . on 21/02/24.
+//
+
+#include "Components.hpp"
---
+++ IKan/Interface/Scene/Components.hpp
@@ -0,0 +1,31 @@
+//
+//  Components.hpp
+//  IKan
+//
+//  Created by Ashish . on 21/02/24.
+//
+
+#pragma once
+
+#include "Core/UUID.hpp"
+
+namespace IKan
+{
+  struct IDComponent
+  {
+    UUID ID = 0;
+  };
+  
+  struct TagComponent
+  {
+    std::string tag;
+    operator std::string& ();
+    operator const std::string& () const;
+  };
+  
+  struct RelationshipComponent
+  {
+    UUID parentHandle = 0;
+    std::vector<UUID> children;
+  };
+} // namespace IKan
