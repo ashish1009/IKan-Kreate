2f3269c2f1eb78051b1b5d95606e83cbe818b8dd
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-23 18:52:08 +0530

    Shading Light
    - Dark for now

--- IKan/Assets/Shaders/PBR_StaticShader.glsl
+++ IKan/Assets/Shaders/PBR_StaticShader.glsl
@@ -39,7 +39,33 @@ struct DirectionLight
 };
 uniform DirectionLight u_DirectionLight;
 
+// Calculate the light factor
+vec3 CalculateLight(vec3 radiance, vec3 L, vec3 F0)
+{
+  return vec3(0.0f);
+}
+
+// Calculate the Direction light
+vec3 CalculateDirectionLight(vec3 F0)
+{
+  return vec3(0.0f);
+}
+
 void main()
 {
-  o_Color = vec4(0.2, 0.2, 0.2, 1.0f);
+  // Calculate reflectance at normal incidence
+  // Note: If dia-electric (like plastic) use F0 of 0.04 and
+  //       If it's a metal, use the albedo color as F0 (metallic workflow)
+  vec3 F0 = vec3(0.04);
+
+  // Calculate the light affect
+  vec3 lightResult = CalculateDirectionLight(F0);
+
+  // Ambient light
+  vec3 ambient = vec3(0.03);
+
+  // Final Light
+  vec3 finalResult = lightResult + ambient;
+  
+  o_Color = vec4(finalResult, 1.0f);
 }
