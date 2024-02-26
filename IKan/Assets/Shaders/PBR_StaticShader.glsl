0b89b1107c8b83a404b68327dafad44abe442f2e
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-02-26 10:14:49 +0530

    Adding Some more params

--- IKan/Assets/Shaders/PBR_StaticShader.glsl
+++ IKan/Assets/Shaders/PBR_StaticShader.glsl
@@ -57,6 +57,8 @@ void main()
 
 layout(location = 0) out vec4 o_Color;
 
+const float PI = 3.14159265359;
+
 in VS_OUT
 {
   vec3 WorldPosition;
@@ -71,6 +73,8 @@ struct PBRParameters
 {
   vec3 View;
   vec3 Normal;
+  float Metalness;
+  float Roughness;
 };
 PBRParameters m_Params;
 
@@ -88,10 +92,20 @@ uniform DirectionLight u_DirectionLight;
 uniform float u_TilingFactor;
 
 // Texture Uniforms
+uniform sampler2D u_AlbedoTexture;
 uniform sampler2D u_NormalTexture;
+uniform sampler2D u_RoughnessTexture;
+uniform sampler2D u_MetallicTexture;
+uniform sampler2D u_DepthTexture;
+uniform sampler2D u_AoTexture;
 
 // Texture Toggle
+uniform float u_AlbedoTextureToggle;
 uniform float u_NormalTextureToggle;
+uniform float u_RoughnessTextureToggle;
+uniform float u_MetallicTextureToggle;
+uniform float u_DepthTextureToggle;
+uniform float u_AoTextureToggle;
 
 // ----------------------------------------------------------------------------
 // Easy trick to get tangent-normals to world-space to keep PBR code simplified.
@@ -109,12 +123,61 @@ vec3 getNormalFromMap(float tilingFactor, vec2 texCoords)
   return normal;
 }
 
+// ----------------------------------------------------------------------------
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+  float r = (roughness + 1.0);
+  float k = (r*r) / 8.0;
+  
+  float nom   = NdotV;
+  float denom = NdotV * (1.0 - k) + k;
+  
+  return nom / denom;
+}
+
+// ----------------------------------------------------------------------------
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+  float a = roughness * roughness;
+  float a2 = a*a;
+  float NdotH = max(dot(N, H), 0.0);
+  float NdotH2 = NdotH*NdotH;
+  
+  float nom   = a2;
+  float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+  denom = PI * denom * denom;
+  
+  return nom / denom;
+}
+
+// ----------------------------------------------------------------------------
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+  float NdotV = max(dot(N, V), 0.0);
+  float NdotL = max(dot(N, L), 0.0);
+  float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+  float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+  
+  return ggx1 * ggx2;
+}
+
+// ----------------------------------------------------------------------------
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+  return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
 // Calculate the light factor
 vec3 CalculateLight(vec3 radiance, vec3 L, vec3 F0)
 {
   vec3 V = m_Params.View;
   vec3 H = normalize(V + L);
   vec3 N = normalize(m_Params.Normal);
+
+  // Cook-Torrance BRDF
+  float NDF = DistributionGGX(N, H, m_Params.Roughness);
+  float G   = GeometrySmith(N, V, L, m_Params.Roughness);
+  vec3  F   = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
 
   return vec3(0.0f);
 }
@@ -136,8 +199,10 @@ void main()
   float tilingFactor = u_TilingFactor;
   vec2 texCoords = vs_Input.TexCoord;
 
-  m_Params.View = normalize(vs_Input.CameraPosition - vs_Input.WorldPosition);
-  m_Params.Normal = getNormalFromMap(tilingFactor, texCoords);
+  m_Params.View         = normalize(vs_Input.CameraPosition - vs_Input.WorldPosition);
+  m_Params.Normal       = getNormalFromMap(tilingFactor, texCoords);
+  m_Params.Metalness    = (u_MetallicTextureToggle > 0.5) ? texture(u_MetallicTexture, texCoords * tilingFactor).r : 0.5f;
+  m_Params.Roughness    = (u_RoughnessTextureToggle > 0.5) ? texture(u_RoughnessTexture, texCoords * tilingFactor).r : 0.5f;
 
   // Calculate reflectance at normal incidence
   // Note: If dia-electric (like plastic) use F0 of 0.04 and
