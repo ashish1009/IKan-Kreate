2c9116e22d9ba4a8dc7f470ff5651fb4f139dd00
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-03-05 10:14:03 +0530

    Creating Link class to link between nodes/Pins

--- IKan/Interface/Editor/NodeEditor/Node.hpp
+++ IKan/Interface/Editor/NodeEditor/Node.hpp
@@ -50,16 +50,16 @@ namespace IKan
   struct Pin
   {
     UUID ID;
-    Node* Node;
-    std::string Name;
-    PinType     Type;
-    PinKind     Kind;
-    StorageKind Storage;
+    Node* node;
+    std::string name;
+    PinType type;
+    PinKind kind;
+    StorageKind storage;
     
-    choc::value::Value Value;
+    choc::value::Value value;
     
     Pin(UUID id, const char* name, PinType type, StorageKind storageKind = StorageKind::Value, choc::value::Value defaultValue = choc::value::Value()) :
-    ID(id), Node(nullptr), Name(name), Type(type), Kind(PinKind::Input), Storage(storageKind), Value(defaultValue)
+    ID(id), node(nullptr), name(name), type(type), kind(PinKind::Input), storage(storageKind), value(defaultValue)
     {
     }
   };
@@ -67,18 +67,33 @@ namespace IKan
   struct Node
   {
     UUID ID;
-    std::string Category, Name;
-    std::vector<Pin> Inputs;
-    std::vector<Pin> Outputs;
-    ImColor Color;
-    NodeType Type;
-    ImVec2 Size;
+    std::string Category, name;
+    std::vector<Pin> inputs;
+    std::vector<Pin> outputs;
+    ImColor color;
+    NodeType type;
+    ImVec2 size;
     
-    std::string State;
-    std::string SavedState;
+    std::string state;
+    std::string savedState;
     
     Node(UUID id, const char* name, ImColor color = ImColor(255, 255, 255)) :
-    ID(id), Name(name), Color(color), Type(NodeType::Blueprint), Size(0, 0)
+    ID(id), name(name), color(color), type(NodeType::Blueprint), size(0, 0)
+    {
+    }
+  };
+
+  struct Link
+  {
+    UUID ID;
+    
+    UUID startPinID;
+    UUID endPinID;
+    
+    ImColor Color;
+    
+    Link(UUID id, UUID startPinId, UUID endPinId) :
+    ID(id), startPinID(startPinId), endPinID(endPinId), color(255, 255, 255)
     {
     }
   };
