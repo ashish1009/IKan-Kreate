c3af454abecc459d12c1edc8a44ce5c77963df60
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-08 18:27:55 +0530

    Creating Interface class for Render APIs

---
+++ IKan/Interface/Renderer/Graphics/RendererAPI.hpp
@@ -0,0 +1,35 @@
+//
+//  RendererAPI.hpp
+//  IKan
+//
+//  Created by Ashish . on 08/07/24.
+//
+
+#pragma once
+
+#include "Renderer/Renderer.hpp"
+#include "Renderer/Graphics/Factories.hpp"
+#include "Renderer/Graphics/Specifications.hpp"
+
+namespace IKan
+{
+  /// This class is the interface for all the renderer APIs
+  class RendererAPI
+  {
+  public:
+    /// This is the default virtual destrutctor
+    virtual ~RendererAPI() = default;
+    
+    /// This function clear all the renderer bits and set the clear color as 'color'
+    /// - Parameter color: new clear color of screen
+    virtual void SetClearColor(const glm::vec4& color) const = 0;
+    /// This function clear all the bits of renderer screen
+    virtual void ClearBits() const = 0;
+    /// This function clears the color bits
+    virtual void ClearColorBits() const = 0;
+    /// This function clears the depth bits
+    virtual void ClearDepthBits() const = 0;
+    /// This function clears the depth bits
+    virtual void ClearStencilBits() const = 0;
+  };
+} // namespace IKan
