509a0a105bf3b6faeba4a23901a2c00e90f67c9e
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-12 14:55:18 +0530

    Creating architecture for pipeline interface class

---
+++ IKan/Interface/Renderer/Graphics/Pipeline.cpp
@@ -0,0 +1,8 @@
+//
+//  Pipeline.cpp
+//  IKan
+//
+//  Created by Ashish . on 12/07/24.
+//
+
+#include "Pipeline.hpp"
---
+++ IKan/Interface/Renderer/Graphics/Pipeline.hpp
@@ -0,0 +1,19 @@
+//
+//  Pipeline.hpp
+//  IKan
+//
+//  Created by Ashish . on 12/07/24.
+//
+
+#pragma once
+
+namespace IKan
+{
+  /// This class is the interface for creating pipeline for renderer
+  class Pipeline
+  {
+  public:
+    /// Default virtual destructor
+    virtual ~Pipeline() = default;
+  };
+} // namespace IKan
