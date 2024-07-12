ee21de2ac963fae5b228bd1afe0776ca58cdf411
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-12 10:25:42 +0530

    Starting Texture interface class

---
+++ IKan/Interface/Renderer/Graphics/Texture.hpp
@@ -0,0 +1,22 @@
+//
+//  Texture.hpp
+//  IKan
+//
+//  Created by Ashish . on 12/07/24.
+//
+
+#pragma once
+
+#include "Renderer/Renderer.hpp"
+#include "Renderer/Graphics/Factories.hpp"
+
+namespace IKan
+{
+  /// This class is the interface for creating texture with specificaion data
+  class Texture 
+  {
+  public:
+    /// Default virtual destructor
+    virtual ~Texture() = default;
+  };
+} //namespace IKan
