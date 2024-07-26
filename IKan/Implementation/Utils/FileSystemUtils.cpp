f83209a0fc59672f1b7b5adaf10dea04005c9800
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-26 15:52:41 +0530

    AddinG APIs in file system utils

--- IKan/Implementation/Utils/FileSystemUtils.cpp
+++ IKan/Implementation/Utils/FileSystemUtils.cpp
@@ -33,4 +33,48 @@ namespace IKan::Utils::FileSystem
     
     return result;
   }
+  bool Copy(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath)
+  {
+    IK_PROFILE();
+    if (std::filesystem::exists(newFilepath))
+    {
+      return false;
+    }
+    
+    std::filesystem::copy(oldFilepath, newFilepath, std::filesystem::copy_options::recursive);
+    return true;
+  }
+  bool Rename(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath)
+  {
+    IK_PROFILE();
+    return Move(oldFilepath, newFilepath);
+  }
+  bool Move(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath)
+  {
+    IK_PROFILE();
+    if (std::filesystem::exists(newFilepath))
+    {
+      return false;
+    }
+    std::filesystem::rename(oldFilepath, newFilepath);
+    return true;
+  }
+  bool MoveFile(const std::filesystem::path& filepath, const std::filesystem::path& dest)
+  {
+    IK_PROFILE();
+    return Move(filepath, dest / filepath.filename());
+  }
+  bool Delete(const std::filesystem::path& filepath)
+  {
+    IK_PROFILE();
+    if (!std::filesystem::exists(filepath))
+    {
+      return false;
+    }
+    if (std::filesystem::is_directory(filepath))
+    {
+      return std::filesystem::remove_all(filepath) > 0;
+    }
+    return std::filesystem::remove(filepath);
+  }
 } // namespace IKan::Utils::FileSystem
--- IKan/Interface/Utils/FileSystemUtils.hpp
+++ IKan/Interface/Utils/FileSystemUtils.hpp
@@ -12,4 +12,27 @@ namespace IKan::Utils::FileSystem
   /// This function returns the absolute File/Directory path for IKan
   /// - Parameter path: Current Relative File/Directory path passed
   std::filesystem::path IKanAbsolute(const std::filesystem::path& path);
+  /// This function Copies the File/Directory
+  /// - Parameters:
+  ///   - oldFilepath: Old Name of File/Directory
+  ///   - newFilepath: New Name of File/Directory
+  bool Copy(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath);
+  /// This function rename the File/Folder
+  /// - Parameters:
+  ///   - oldName: Old Name of File/Folder
+  ///   - newName: New Name of File/Folder
+  bool Rename(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath);
+  /// This function Moves the File/Directory
+  /// - Parameters:
+  ///   - oldFilepath: Old Name of File/Directory
+  ///   - newFilepath: New Name of File/Directory
+  bool Move(const std::filesystem::path& oldFilepath, const std::filesystem::path& newFilepath);
+  /// This function moves the file
+  /// - Parameters:
+  ///   - filepath: source
+  ///   - dest: destination
+  bool MoveFile(const std::filesystem::path& filepath, const std::filesystem::path& dest);
+  /// This function deletes the File/Directory
+  /// - Parameter filepath: File/Directory path to be deleted
+  bool Delete(const std::filesystem::path& filepath);
 } // namespace IKan::Utils
