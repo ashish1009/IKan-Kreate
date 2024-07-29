38daa21a77fd1cb35ed0a4f8b9affc82bd0ba5f9
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-29 18:15:54 +0530

    Creating CB action result class

--- Kreator/Kreator/Editor/ContentBrowserItem.cpp
+++ Kreator/Kreator/Editor/ContentBrowserItem.cpp
@@ -6,3 +6,25 @@
 //
 
 #include "ContentBrowserItem.hpp"
+
+namespace Kreator
+{
+  static char s_renameBuffer[MAX_INPUT_BUFFER_LENGTH];
+  
+  void CBItemActionResult::Set(ContentBrowserAction flag, bool value)
+  {
+    if (value)
+    {
+      Field |= (uint16_t)flag;
+    }
+    else
+    {
+      Field &= ~(uint16_t)flag;
+    }
+  }
+  
+  bool CBItemActionResult::IsSet(ContentBrowserAction flag) const
+  {
+    return (uint16_t)flag & Field;
+  }
+} // namespace Kreator
--- Kreator/Kreator/Editor/ContentBrowserItem.hpp
+++ Kreator/Kreator/Editor/ContentBrowserItem.hpp
@@ -20,4 +20,40 @@ namespace Kreator
     std::vector<AssetHandle> assets;
     std::map<AssetHandle, Ref<DirectoryInfo>> subDirectories;
   };
+  
+  enum class ContentBrowserAction
+  {
+    None                = 0,
+    Refresh             = BIT(0),
+    ClearSelections     = BIT(1),
+    Selected            = BIT(2),
+    Hovered             = BIT(3),
+    Renamed             = BIT(4),
+    NavigateToThis      = BIT(5),
+    OpenDeleteDialogue  = BIT(6),
+    SelectToHere        = BIT(7),
+    Moved               = BIT(8),
+    ShowInExplorer      = BIT(9),
+    OpenExternal        = BIT(10),
+    Reload              = BIT(11),
+    Copy                = BIT(12),
+    Duplicate           = BIT(13),
+    StartRenaming       = BIT(14)
+  };
+  
+  /// This structure stores all the actions for an item in CBP
+  struct CBItemActionResult
+  {
+    uint16_t Field = 0;
+    
+    /// This function set an action in CBI
+    /// - Parameters:
+    ///   - flag: Action flag
+    ///   - value: Set reset value
+    void Set(ContentBrowserAction flag, bool value);
+    /// This function check if an item have an action
+    /// - Parameter flag: action flag
+    bool IsSet(ContentBrowserAction flag) const;
+  };
+
 } // namespace Kreator
