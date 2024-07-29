6153a045894d625461718edc791d4e5baf564e36
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-07-29 18:26:29 +0530

    Creating selection context class

--- Kreator/Kreator/Panels/ContentBrowserPanel.cpp
+++ Kreator/Kreator/Panels/ContentBrowserPanel.cpp
@@ -6,3 +6,78 @@
 //
 
 #include "ContentBrowserPanel.hpp"
+
+namespace Kreator
+{
+  static float s_padding = 2.0f;
+  
+  // Selection Stack ------------------------------------------------------------------------------------------------
+  SelectionStack::SelectionStack()
+  : VectorIterator<AssetHandle>(m_selections)
+  {
+    
+  }
+  void SelectionStack::CopyFrom(const SelectionStack& other)
+  {
+    m_selections.assign(other.begin(), other.end());
+  }
+  
+  void SelectionStack::Select(AssetHandle handle)
+  {
+    if (IsSelected(handle))
+    {
+      return;
+    }
+    m_selections.push_back(handle);
+  }
+  
+  void SelectionStack::Deselect(AssetHandle handle)
+  {
+    if (!IsSelected(handle))
+    {
+      return;
+    }
+
+    for (auto it = m_selections.begin(); it != m_selections.end(); it++)
+    {
+      if (handle == *it)
+      {
+        m_selections.erase(it);
+        break;
+      }
+    }
+  }
+  
+  bool SelectionStack::IsSelected(AssetHandle handle) const
+  {
+    for (const auto& selectedHandle : m_selections)
+    {
+      if (selectedHandle == handle)
+      {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  void SelectionStack::Clear()
+  {
+    m_selections.clear();
+  }
+  
+  size_t SelectionStack::SelectionCount() const
+  {
+    return m_selections.size();
+  }
+  const AssetHandle* SelectionStack::SelectionData() const
+  {
+    return m_selections.data();
+  }
+  
+  AssetHandle SelectionStack::operator[](size_t index) const
+  {
+    IK_ASSERT(index >= 0 and index < m_selections.size());
+    return m_selections[index];
+  }
+} // namespace Kreator
--- Kreator/Kreator/Panels/ContentBrowserPanel.hpp
+++ Kreator/Kreator/Panels/ContentBrowserPanel.hpp
@@ -5,9 +5,41 @@
 //  Created by Ashish . on 29/07/24.
 //
 
-#ifndef ContentBrowserPanel_hpp
-#define ContentBrowserPanel_hpp
+#pragma once
 
-#include <stdio.h>
+#include "ContentBrowserItem.hpp"
 
-#endif /* ContentBrowserPanel_hpp */
+namespace Kreator
+{
+  /// This class stores the selection Items in a stack
+  class SelectionStack : public VectorIterator<AssetHandle>
+  {
+  public:
+    /// Default constructor
+    SelectionStack();
+    
+    /// This fucntion copy the selection
+    /// - Parameter other: other selection stack
+    void CopyFrom(const SelectionStack& other);
+    /// This function select a item
+    /// - Parameter handle: item handle
+    void Select(AssetHandle handle);
+    /// This function deselect a item
+    /// - Parameter handle: item handle
+    void Deselect(AssetHandle handle);
+    /// This function clears the item selection
+    void Clear();
+    /// This function checks if item is selectd
+    /// - Parameter handle: item handle
+    bool IsSelected(AssetHandle handle) const;
+    /// This function returns the selection counts
+    size_t SelectionCount() const;
+    /// This function returns the selection data
+    const AssetHandle* SelectionData() const;
+    /// This function returns the item at index
+    AssetHandle operator[](size_t index) const;
+    
+  private:
+    std::vector<AssetHandle> m_selections;
+  };
+} // namespace Kreator
