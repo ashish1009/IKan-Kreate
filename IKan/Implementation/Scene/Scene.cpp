963d5b67c1e0b771b8ac33e42acb6f777850c527
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-08-05 11:44:55 +0530

    Copy and duplicate camera component

--- IKan/Implementation/Scene/Scene.cpp
+++ IKan/Implementation/Scene/Scene.cpp
@@ -121,7 +121,8 @@ namespace IKan
     CopyComponent<TagComponent>(target->m_registry, m_registry, enttMap);
     CopyComponent<RelationshipComponent>(target->m_registry, m_registry, enttMap);
     CopyComponent<TransformComponent>(target->m_registry, m_registry, enttMap);
-    
+    CopyComponent<CameraComponent>(target->m_registry, m_registry, enttMap);
+
     // Sort IdComponent by by entity handle (which is essentially the order in which they were created)
     // This ensures a consistent ordering when iterating IdComponent (for example: when rendering scene hierarchy panel)
     target->m_registry.sort<IDComponent>([&target](const auto lhs, const auto rhs)
@@ -254,7 +255,8 @@ namespace IKan
     
     CopyComponentIfExists<VisibilityComponent>(newEntity.m_entityHandle, entity.m_entityHandle, m_registry);
     CopyComponentIfExists<TransformComponent>(newEntity.m_entityHandle, entity.m_entityHandle, m_registry);
-    
+    CopyComponentIfExists<CameraComponent>(newEntity.m_entityHandle, entity.m_entityHandle, m_registry);
+
     auto childIds = entity.Children();
     for (auto childId : childIds)
     {
