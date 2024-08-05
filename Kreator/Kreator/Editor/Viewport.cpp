6a4bb62003a109d7d906544539391d3db57bc014
Ashish <22274092+ashish1009@users.noreply.github.com>
2024-08-05 17:25:46 +0530

    Viewport mouse APIs

--- Kreator/Kreator/Editor/Viewport.cpp
+++ Kreator/Kreator/Editor/Viewport.cpp
@@ -6,3 +6,24 @@
 //
 
 #include "Viewport.hpp"
+
+namespace Kreator
+{
+  void Viewport::GetMouseViewportSpace()
+  {
+    IK_PERFORMANCE("Viewport::GetMouseViewportSpace");
+    auto [mx, my] = ImGui::GetMousePos();
+    const auto& viewportBounds = bounds;
+    mx -= viewportBounds[0].x;
+    my -= viewportBounds[0].y;
+    auto viewportWidth = viewportBounds[1].x - viewportBounds[0].x;
+    auto viewportHeight = viewportBounds[1].y - viewportBounds[0].y;
+    
+    mouseViewportSpace = { (mx / viewportWidth) * 2.0f - 1.0f, ((my / viewportHeight) * 2.0f - 1.0f) * -1.0f };
+  }
+  bool Viewport::IsMouseSpaceInViewport()
+  {
+    return (mouseViewportSpace.spaceMouseX >= -1.0f and mouseViewportSpace.spaceMouseX <= 1.0f and
+            mouseViewportSpace.spaceMouseY >= -1.0f and mouseViewportSpace.spaceMouseY <= 1.0f);
+  }
+} // namespace Kreator
--- Kreator/Kreator/Editor/Viewport.hpp
+++ Kreator/Kreator/Editor/Viewport.hpp
@@ -12,9 +12,23 @@ namespace Kreator
   /// This structure store the kretor viewport data
   struct Viewport
   {
+    /// This structure stores the mouse space in viewport
+    struct MouseViewportSpace
+    {
+      float spaceMouseX, spaceMouseY;
+    };
+
     bool isHovered {false};
     bool isFocused {false};
     
     uint32_t width {0}, height {0};
+    
+    glm::vec2 bounds[2];
+    MouseViewportSpace mouseViewportSpace;
+    
+    /// This function returns the space mouse position
+    void GetMouseViewportSpace();
+    /// This function check the mouse position inside viewport
+    bool IsMouseSpaceInViewport();
   };
 } // namespace Kreator
